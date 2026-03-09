8765c4928d90e54c6d7480bbe0afde6ec6905f05
Tiooprime2 <hrid9112@gmail.com>
2026-03-09 16:53:29 +0800

    Add files via upload

--- main.lua
+++ main.lua
@@ -4,18 +4,11 @@
 
 local BASE_URL = "https://raw.githubusercontent.com/Tiooprime2/Ninja-legend/refs/heads/main/"
 
-local UI        = loadstring(game:HttpGet(BASE_URL .. "SemuaUI/ui.lua"))()
-local ESP       = loadstring(game:HttpGet(BASE_URL .. "MainFeat/esp.lua"))()
-local Island    = loadstring(game:HttpGet(BASE_URL .. "MainFeat/island.lua"))()
-local Element   = loadstring(game:HttpGet(BASE_URL .. "MainFeat/element.lua"))()
-local AutoSwing = loadstring(game:HttpGet(BASE_URL .. "MainFeat/autoswing.lua"))()
-local AutoSell  = loadstring(game:HttpGet(BASE_URL .. "MainFeat/autosell.lua"))()
-local AutoBuy   = loadstring(game:HttpGet(BASE_URL .. "MainFeat/autobuysword.lua"))()
+local UI      = loadstring(game:HttpGet(BASE_URL .. "SemuaUI/ui.lua"))()
+local Main    = loadstring(game:HttpGet(BASE_URL .. "MainFeat/main_tab.lua"))()
+local Island  = loadstring(game:HttpGet(BASE_URL .. "MainFeat/island.lua"))()
+local Element = loadstring(game:HttpGet(BASE_URL .. "MainFeat/element.lua"))()
 
--- Sambungkan ke UI
-Island.init(UI.scroll, UI.THEME, UI.tween, UI.corner, UI.stroke, UI.mainGui, UI.onClose)
-Element.init(UI.scroll, UI.THEME, UI.tween, UI.corner, UI.stroke, UI.mainGui, UI.onClose)
-ESP.init(UI.scroll, UI.THEME, UI.tween, UI.corner, UI.stroke)
-AutoSwing.init(UI.scroll, UI.THEME, UI.tween, UI.corner, UI.stroke)
-AutoSell.init(UI.scroll, UI.THEME, UI.tween, UI.corner, UI.stroke)
-AutoBuy.init(UI.scroll, UI.THEME, UI.tween, UI.corner, UI.stroke)
+Main.init(UI.mainPage, UI.THEME, UI.tween, UI.corner, UI.stroke, UI.mainGui, UI.onClose)
+Island.init(UI.islandPage, UI.THEME, UI.tween, UI.corner, UI.stroke, UI.mainGui)
+Element.init(UI.elementPage, UI.THEME, UI.tween, UI.corner, UI.stroke, UI.mainGui)
